968cdeef27abd0b6b5313830b451885bbb0713f0
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-07-05 02:52:41 +0900

    Change sidebar sociallinks

--- src/components/sidebar/SocialLinks.js
+++ src/components/sidebar/SocialLinks.js
@@ -12,36 +12,44 @@ import "./sidebar.css"
 const SocialLinks = ({ contacts }) => {
     return (
         <div className="side-social-links float-left mt-3 mb-3">
+            {/*
             <a className="text-secondary p-2"
                 href={contacts.linkedin}>
                 <span title="Linked In">
                     <FaLinkedin size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
+            */}
             <a className="text-secondary p-2"
                 href={contacts.github}>
                 <span title="GitHub">
                     <FaGithubSquare size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
+            {/*
             <a className="text-secondary p-2"
                 href={contacts.stackoverflow}>
                 <span title="Stack Overflow">
                     <FaStackOverflow size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
+            */}
+            {/*
             <a className="text-secondary p-2"
                 href={contacts.freecodecamp}>
                 <span title="freeCodeCamp">
                     <FaFreeCodeCamp size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
+            */}
+            {/*
             <a className="text-secondary p-2"
                 href={contacts.twitter}>
                 <span title="Twitter">
                     <FaTwitterSquare size={26} style={{ color: "secondary" }} />
                 </span>
             </a>
+            */}
         </div>
     )
 }
